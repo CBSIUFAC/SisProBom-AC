3dddaeafe53107a79d377a847f541feda355d7c0
Daniel Silva <danielnsilva@gmail.com>
2015-01-15 01:17:44 -0500

    Modificações na view Setor (CRUD completo)
    @SetorBean.java
    - As funções inserir e editar estão em um mesmo método.
    - O método 'deletarSetor' foi removido, pois não é necessário. O método
    'deletarSetores' funciona para 1 ou mais registros.
    - A remoção de setores retorna mensagens de sucesso ou erro.
    - Foi criado um método para inicializar o objeto setor com o registro
    selecionado na tabela para posterior edição.
    - Também foi criado um método que anula o objeto setor quando a edição é
    cancelada, fazendo com que assim o campo no formulário fique em branco.
    
    @setor.xhtml
    - Com um duplo clique em uma das linhas o formulário é carregado com o
    dados do registro selecionado para edição.
    - As messagens agora são gerenciadas por um componente 'growl'.
    - O botão cancelar, além de anular o objeto, também limpa o formulário,
    evitando que marcações de erro (como campo inválidos) permaneçam quando
    o 'dialog' for novamente chamado.

--- protocolo/src/main/java/managedBean/SetorBean.java
+++ protocolo/src/main/java/managedBean/SetorBean.java
@@ -7,7 +7,10 @@ import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
 import javax.faces.context.FacesContext;
+
 import org.hibernate.JDBCException;
+import org.primefaces.context.RequestContext;
+import org.primefaces.event.SelectEvent;
 
 import DAO.SetorDAO;
 import entity.Setor;
@@ -31,12 +34,21 @@ public class SetorBean implements Serializable {
 		this.setor = setor;
 	}
 	
-	public void inserirSetor() {
+	public void salvarSetor() {
 		try {
-			dao.inserirSetor(setor);
+			String textoMsg = null;
+			if (setor.getId() == 0) {
+				dao.inserirSetor(setor);
+				textoMsg = "Registro incluído com sucesso!";
+			} else {
+				dao.atualizarSetor(setor);
+				textoMsg = "Registro alterado com sucesso!";
+				RequestContext.getCurrentInstance().execute("PF('dlgCadastro').hide()");
+				selecionados = null;
+			}
 			listaSetor = null;
 			setor = null;
-			FacesMessage msg = new FacesMessage(FacesMessage.SEVERITY_INFO, null, "Registro incluído com sucesso!");
+			FacesMessage msg = new FacesMessage(FacesMessage.SEVERITY_INFO, null, textoMsg);
 			FacesContext.getCurrentInstance().addMessage(null, msg);
 		} catch (JDBCException e) {
 			FacesMessage msg = new FacesMessage(FacesMessage.SEVERITY_ERROR, null, e.getSQLException().getMessage());
@@ -44,18 +56,19 @@ public class SetorBean implements Serializable {
 		}
 	}
 	
-	public String deletarSetor() {
-		dao.deletarSetor(setor);
-		listaSetor = null;
-		return "setor";
-	}
-	
 	public void deletarSetores() {
 		if (selecionados.length > 0) {
-			for (Setor setor : selecionados) {
-				dao.deletarSetor(setor);
+			try {
+				for (Setor setor : selecionados) {
+					dao.deletarSetor(setor);
+				}
+				listaSetor = null;
+				FacesMessage msg = new FacesMessage(FacesMessage.SEVERITY_INFO, null, "Registro removido com sucesso!");
+				FacesContext.getCurrentInstance().addMessage(null, msg);
+			} catch (JDBCException e) {
+				FacesMessage msg = new FacesMessage(FacesMessage.SEVERITY_ERROR, null, e.getSQLException().getMessage());
+				FacesContext.getCurrentInstance().addMessage(null, msg);
 			}
-			listaSetor = null;
 		}
 	}
 	
@@ -71,6 +84,14 @@ public class SetorBean implements Serializable {
 
 	public void setSelecionados(Setor[] selecionados) {
 		this.selecionados = selecionados;
+	}
+	
+	public void preparaEdicao(SelectEvent e) {
+		setor = (Setor) e.getObject();
+	}
+	
+	public void cancelar() {
+		setor = null;
 	}
 
 }
