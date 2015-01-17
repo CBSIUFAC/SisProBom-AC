3209e15bc34ec2f5d40b59496b8bfdc4ddf456b1
Saii <studionoronha@yahoo.com>
2015-01-18 04:48:25 +0800

    Alterado, precisa de modificação.

--- protocolo/src/main/java/util/funcionarioConverter.java
+++ protocolo/src/main/java/util/funcionarioConverter.java
@@ -6,12 +6,15 @@ import javax.faces.convert.FacesConverter;
 
 import org.omnifaces.converter.SelectItemsConverter;
 
+
+
 import entity.Funcionario;
+import entity.Setor;
 
 @FacesConverter("funcionarioConverter")
 public class funcionarioConverter extends SelectItemsConverter {
 	public String getAsObject(FacesContext context, UIComponent component, Object value) {
-        Integer matricula = (value instanceof Funcionario) ? ((Funcionario) value).getMatricula() : null;
-        return (matricula != null) ? String.valueOf(matricula) : null;
+        Integer id_funcionario = (value instanceof Funcionario) ? ((Funcionario) value).getId_funcionario()() : null;
+        return (id_funcionario != null) ? String.valueOf(id_funcionario) : null;
     }
 }
